81c50bd5c8c4de6c6ac088623ce6c5ffd895050b
CharlesChanLok <charleschanlok@gmail.com>
2018-03-22 15:52:33 +0800

    added router

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const Router = require('router');
 const cors = require('cors');
 const app = express();
 
