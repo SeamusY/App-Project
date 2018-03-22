745cc88dbcd1c2dbac44fdb056b88804ac8a44ef
CharlesChanLok <charleschanlok@gmail.com>
2018-03-22 15:45:56 +0800

    added basic nodejs template

---
+++ server/knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: process.env.prod_DB_NAME,
+      user: process.env.prod_DB_USERNAME,
+      password: process.env.prod_DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
