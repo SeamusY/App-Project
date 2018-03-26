e60d14864144e5f2988088e65bcd35c9ab7ba030
CharlesChanLok <charleschanlok@gmail.com>
2018-03-26 15:29:04 +0800

    fix server-config rename error

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = require("./config/router");
 const guard = require("./config/guard");
-const { PORT, NODE_ENV } = require('./config/server');
+const { PORT, NODE_ENV } = require('./config/server-config');
 const cors = require('cors');
 const app = express();
 
@@ -17,5 +17,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // app.use("/user",);
 
+app.get('/', (req, res) => {
+    res.send('working');
+})
 app.listen(PORT, () => console.log('Listening on :', PORT));
 
