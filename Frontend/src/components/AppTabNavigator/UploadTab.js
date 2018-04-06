b8663ba8bfd35f485b61fa37a489cbf6406e7ed4
CharlesChanLok <charleschanlok@gmail.com>
2018-04-06 20:04:38 +0800

    commit

--- Frontend/src/components/AppTabNavigator/UploadTab.js
+++ Frontend/src/components/AppTabNavigator/UploadTab.js
@@ -4,37 +4,88 @@ import {
   StyleSheet,
   Text,
   View,
-  ScrollView
+  ScrollView,
+  TouchableOpacity
 } from 'react-native';
 import { Icon } from 'native-base';
 
+import ImagePicker from 'react-native-image-picker';
 
+const options = {
+  title: 'Select a photo',
+  takePhotoButtonTitle: 'Take a photo',
+  chooseFromLibraryButtonTitle: 'Choose from gallery',
+  qality: 1
+
+};
 class UploadTab extends Component {
 
+  constructor () {
+    super()
+    this.state = {
+      imageSource: null
+    }
+  }
+  selectPhoto () {
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };
+
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+          imageSource: source
+        });
+      }
+    });
+  }
   render() {
     return (
        <ScrollView>
-         <Text>
-             Upload Photo
-          </Text>
-        </ScrollView>
- 
+        <View style={styles.container}>
+        <Image style={style.image}
+        source={this.state.imageSource != null }
+        />
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.text}>Select</Text>
+          </TouchableOpacity>
 
-
-        );
+        </View>
+       </ScrollView>
+    );
   }
 }
 
 export default UploadTab;
 
 const styles = StyleSheet.create({
-          container: {
-          flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  button: {
+    width: 250,
+    height: 50,
+    backgroundColor: '#fff'
+  },
+  text: {
+    color: 'pink',
+    fontSize: 30,
+    textAlign: 'center'
+  }
 });
 
 
