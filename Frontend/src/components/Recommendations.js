d7331dc55976a9e031ed7d3622fbe4ac08077115
Hugo Cheng <hugocheng6565@gmail.com>
2018-04-10 10:35:38 +0800

    min change

--- Frontend/src/components/Recommendations.js
+++ Frontend/src/components/Recommendations.js
@@ -163,7 +163,7 @@ export default class Recommendations extends Component {
                     </Card>
 
                 </Content>
-                {/* </Container> */}
+               
             </ScrollView>
 
 
