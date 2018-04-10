2ec8171e43627202a335cd8e0152bfc44624dd6f
CharlesChanLok <charleschanlok@gmail.com>
2018-04-10 17:42:53 +0800

    added serving images

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,12 @@ const guard = require("./config/guard");
 const { PORT, NODE_ENV } = require('./config/server-config');
 const cors = require('cors');
 const app = express();
-
 const photoRouter = require('./routers/photoRouter');
 const knexFile = require('./knexfile')[NODE_ENV]
 const knex = require('knex')(knexFile)
 
+//serve the images
+app.use(express.static('uploads'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
