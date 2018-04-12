075094abc1974d56af4e5a8da7aef61a42be0793
CharlesChanLok <charleschanlok@gmail.com>
2018-04-12 14:17:33 +0800

    turn photorouter to class

--- Backend/routers/PhotoRouter.js
+++ Backend/routers/PhotoRouter.js
@@ -3,40 +3,47 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 
-isDirExist = (req, res, next) => {
-  fs.stat(path.join(__dirname, `../uploads/${req.params.id}`), (err) => {
-    if (!err) {
-      next();
-    }
-    fs.mkdir(path.join(__dirname, `../uploads/${req.params.id}`), (err) => {
-      if (err) {
-        console.log('2', err)
-      }
-      next()
-    })
-  })
-}
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    console.log(req);
-    cb(null, path.join(`uploads/${req.params.id}`))
-  },
-  filename: function (req, file, cb) {
-    console.log(file.fieldname);
-    cb(null, file.fieldname + '-' + Date.now())
-  }
+    destination: function (req, file, cb) {
+        console.log(req);
+        cb(null, path.join(`uploads/${req.params.id}`))
+    },
+    filename: function (req, file, cb) {
+        console.log(file.fieldname);
+        cb(null, file.fieldname + '-' + Date.now())
+    }
 })
 
-var upload = multer({ storage: storage })
+module.exports = class PhotoRouter {
+    constructor() {
+        // this.photoService = photoService;
+        this.upload = multer({ storage: storage })
+    }
 
-let router = express.Router();
+    // check if the directory existed 
+    isDirExist (req, res, next) {
+        fs.stat(path.join(__dirname, `../uploads/${req.params.id}`), (err) => {
+            if (!err) {
+                next();
+            }
+            fs.mkdir(path.join(__dirname, `../uploads/${req.params.id}`), (err) => {
+                if (err) {
+                    console.log('2', err)
+                }
+                next()
+            })
+        })
+    }
 
-router.post('/:id', isDirExist, upload.single('image'), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-  console.log(req.body)
-  console.log(req.file)
-  res.status(201).send('success')
-})
+    router() {
+        let router = express.Router();
+        router.post('/:id', this.isDirExist, this.upload.single('image'), this.post.bind(this));
+        return router;
+    }
 
-module.exports = router;
+    post(req, res){
+        console.log(req.body)
+        console.log(req.file)
+        return res.status(201).send('success')
+    }
+} 
