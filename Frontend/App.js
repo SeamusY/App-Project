ebe512811a2533f713b0074e6960eb91ad41e9b0
Telford Ho <telfordho@gmai.com>
2018-04-13 23:04:26 +0800

    initial Page change back to login

--- Frontend/App.js
+++ Frontend/App.js
@@ -70,7 +70,7 @@ const AppStackNavigator = StackNavigator({
   
 },
   {
-    initialRouteName: 'Main',
+    initialRouteName: 'Login',
     // headerMode: 'screen',
     // navigationOptions: {
     //     headerVisible: false,
