ac8919a06288dad957009b8c7862a7538f6cdfb4
CharlesChanLok <charleschanlok@gmail.com>
2018-04-13 18:14:59 +0800

    fixed photo router bug

--- Backend/routers/PhotoRouter.js
+++ Backend/routers/PhotoRouter.js
@@ -22,35 +22,51 @@ module.exports = class PhotoRouter {
                     cb(null, path.join(`uploads/${req.params.id}`))
                 },
                 filename: function (req, file, cb) {
-                    cb(null, file.fieldname + '-' + Date.now())
-                }
+                    console.log(file);
+                    fs.stat(path.join(__dirname, `../uploads/${req.params.id}`), (err) => {
+                        if (!err) {
+                            cb(null, file.fieldname + '-' + Date.now())
+                        }
+                        fs.mkdir(path.join(__dirname, `../uploads/${req.params.id}`), (err) => {
+                            if (err) {
+                                console.log('2', err)
+                            }
+                            cb(null, file.fieldname + '-' + Date.now())
+                        })
+                    })
+                    
+                },
+
             })
-        })
+        });
+
+        //this.upload = multer({dest: "uploads/"})
     }
 
     // check if the directory existed 
-    isDirExist(req, res, next) {
-        fs.stat(path.join(__dirname, `../uploads/${req.params.id}`), (err) => {
-            if (!err) {
-                next();
-            }
-            fs.mkdir(path.join(__dirname, `../uploads/${req.params.id}`), (err) => {
-                if (err) {
-                    console.log('2', err)
-                }
-                next()
-            })
-        })
-    }
+    // isDirExist (req, res, next) {
+    //     fs.stat(path.join(__dirname, `../uploads/${req.params.id}`), (err) => {
+    //         if (!err) {
+    //             next();
+    //         }
+    //         fs.mkdir(path.join(__dirname, `../uploads/${req.params.id}`), (err) => {
+    //             if (err) {
+    //                 console.log('2', err)
+    //             }
+    //             next();
+    //         })
+    //     })
+    // }
 
-    router() {
+    router () {
         let router = express.Router();
-        router.post('/:id', this.isDirExist, this.upload.single('image'), this.post.bind(this));
+        router.get('/', this.upload.single('image'),  this.post.bind(this));
+        router.post('/:id', this.upload.single('image'), this.post.bind(this));
         return router;
     }
 
-    post(req, res) {    
-        console.log(file)
+    post (req, res) {    
+        console.log(req.file)
         return res.status(201).send('success')
     }
 } 
